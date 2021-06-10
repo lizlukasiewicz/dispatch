905b8411c2ad9e5fea572588bca5affda239be99
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-06-09 20:17:55 -0400

    huge bug, using mapbox lesson as playground and pasting working code here

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const express = require('express')
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN })
+const layouts =require('express-ejs-layouts')
+const chalk = require('chalk')
+
+//CONFIG APP
+const app = express()
+const PORT = process.env.PORT || 5000
+app.set('view engine', 'ejs')
+
+//MIDDLEWARES
+app.use(layouts)
+app.use(express.static(__dirname + `/public`))
+
+//  ROUTES
+
+//  GET / ----show a form that lets the user search for a location
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+//   GET /SEARCH -- GEOCODE user form data and render a map --(response.body.features[0])maps the center of the city
+app.get('/search', (req, res) => {
+    geocodingClient.forwardGeocode({
+        query: `${req.query.city}, ${req.query.state}`
+    })
+    .send()
+    .then(response => {
+        console.log(response.body.features[0])
+        res.render('show.ejs', {
+            match: response.body.features[0],
+            mapkey: process.env.MAPBOX_TOKEN
+        })
+    })
+})
+
+app.listen(PORT, () => console.log(chalk.bold.rgb(10,100,200)`smooth sounds of port ${PORT}🗺`))
+
+// console.log(chalk`{bold.rgb(10,100,200) Hello!}`)
+// console.log(chalk.bold.rgb(10, 100, 200)('Hello!'));
+// console.log(chalk.bold.rgb(10, 100, 200)`Hello!`)
+//chalk.bgKeyword('orange')('Some orange text'
