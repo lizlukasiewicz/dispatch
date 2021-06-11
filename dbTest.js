3ca31f29f068248e88db04f95b848709d254b9db
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-06-11 19:46:43 -0400

    bug connecting tables to database, started template for customer add/edit/view

---
+++ dbTest.js
@@ -0,0 +1,18 @@
+const db = require('./models')
+
+// db.customer.create({
+//   phone: 9175478361,
+//   firstName: 'Yubaba',
+//   lastName: 'zeniba'
+// })
+// .then(customer => {
+//   console.log(customer.get())
+// })
+
+// db.article.findOne({
+//   where: { id: 1 },
+//   include: [db.comment]
+// }).then(article => {
+//   // by using eager loading, the article model should have a comments key
+//   console.log(article.comments)
+// })
